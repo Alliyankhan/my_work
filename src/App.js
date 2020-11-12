152a003d16d0022f264c4c908b85882b3e5bb300
Alliyankhan <64958767+Alliyankhan@users.noreply.github.com>
2020-11-12 18:04:36 +0500

    Edit
    add dinner.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function App() {
-  return <h1>Hello World</h1>
+  return (
+      <div className='App' >
+        <Dinner material= 'web development'/> {/* <Dinner></Dinner> */}
+      </div>
+  );
 }
 
 export default App;
